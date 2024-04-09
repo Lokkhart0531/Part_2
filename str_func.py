70d9b80b4c158eb4bf13386214478cdb50b5a224
Valeriya Ryabkova <larioja0531@gmail.com>
2024-04-10 00:07:31 +0300

    add docstring in str

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(some_str):
+    """
+    Return str in upper
+    """
     return some_str.upper()
+
+def str_title(some_str):
+    """
+    Return some_str in title
+    """
+    return some_str.title()
